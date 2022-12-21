c272a951f98e81007e9e7c2508a1d5d55228e271
Richard Lin <richardlin4375@gmail.com>
2022-12-21 14:52:47 -0500

    Healthymind

--- index.js
+++ index.js
@@ -1,76 +1,143 @@
+window.onload = function(){document.getElementById("bg").style = "opacity: 0";};
+var healthymind = document.getElementById("healthymind");
+var fluorescence = document.getElementById("fluorescence");
+var epanos = document.getElementById("epanos");
+var websymphonies = document.getElementById("websymphonies");
+
 var decrease = setInterval(function () {
     if (window.scrollY > 50 && window.scrollY < 450) {
-        document.getElementById("healthymind").style = "width: 300px; height: 400px;"; 
+        healthymind.style = "width: 300px; height: 400px;"; 
         document.getElementById("healthymind-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
     }
     if (window.scrollY < 50) {
-        document.getElementById("healthymind").style = "width: 1000px; height: 600px;"; 
+        healthymind.style = "width: 1000px; height: 600px;"; 
         document.getElementById("healthymind-name").style = "letter-spacing: 35px; font-size: 50px; color: white;";
     }
 
     if (window.scrollY > 450 && window.scrollY < 900) {
-        document.getElementById("fluorescence").style = "width: 300px; height: 400px;"; 
+        fluorescence.style = "width: 300px; height: 400px;"; 
         document.getElementById("fluorescence-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
     }
     if (window.scrollY < 450 && window.scrollY > 50) {
-        document.getElementById("fluorescence").style = "width: 1000px; height: 600px;"; 
+        fluorescence.style = "width: 1000px; height: 600px;"; 
         document.getElementById("fluorescence-name").style = "letter-spacing: 35px; font-size: 50px; color: white;";
     }
 
     if (window.scrollY > 900 && window.scrollY < 1200) {
-        document.getElementById("epanos").style = "width: 300px; height: 400px;"; 
+        epanos.style = "width: 300px; height: 400px;"; 
         document.getElementById("epanos-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
         
     }
     if (window.scrollY < 900 && window.scrollY > 450) {
-        document.getElementById("epanos").style = "width: 1000px; height: 600px;"; 
+        epanos.style = "width: 1000px; height: 600px;"; 
         document.getElementById("epanos-name").style = "letter-spacing: 35px; font-size: 50px; color: white;";
     }
 
     if (window.scrollY > 1200) {
-        document.getElementById("websymphonies").style = "width: 100px; height: 150px;"; 
+        websymphonies.style = "width: 100px; height: 150px;"; 
         document.getElementById("websymphonies-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
         
     }
     if (window.scrollY < 1200 && window.scrollY > 900) {
-        document.getElementById("websymphonies").style = "width: 1000px; height: 600px;"; 
+        websymphonies.style = "width: 1000px; height: 600px;"; 
         document.getElementById("websymphonies-name").style = "letter-spacing: 35px; font-size: 50px; color: white;";
     }
 
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
         disableScrolling();
         clearInterval(decrease);
-        document.getElementById("healthymind").style = "width: 100px; height: 150px; margin-top: 10px;"; 
-        document.getElementById("fluorescence").style = "width: 100px; height: 150px; margin-top: 10px;"; 
-        document.getElementById("epanos").style = "width: 100px; height: 150px; margin-top: 10px;"; 
-        document.getElementById("websymphonies").style = "width: 100px; height: 150px; margin-top: 10px;"; 
+        document.getElementById("healthymind-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
+        document.getElementById("fluorescence-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
+        document.getElementById("epanos-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
+        document.getElementById("websymphonies-name").style = "letter-spacing: 0px; font-size: 0px; color: transparent;";
+        healthymind.style = "width: 100px; height: 150px; margin-top: 10px;"; 
+        fluorescence.style = "width: 100px; height: 150px; margin-top: 10px;"; 
+        epanos.style = "width: 100px; height: 150px; margin-top: 10px;"; 
+        websymphonies.style = "width: 100px; height: 150px; margin-top: 10px;"; 
         document.getElementById("all").style = "width: 100px; margin-left: 20%";
         document.getElementById("bottom").style = "height: 0px;"; 
-        sleep(2000).then(() => { 
+        sleep(1500).then(() => { 
             document.getElementById("all").style = "width: 60%; margin-left: 20%";
-            document.getElementById("websymphonies").style = "width: 100px; height: 150px; margin-top: 10px; margin-left: 70%;";
-            document.getElementById("epanos").style = "width: 100px; height: 150px; margin-top: 10px; margin-left: 25%;";
-            document.getElementById("fluorescence").style = "width: 100px; height: 150px; margin-top: 10px; margin-left: -25%;";
-            document.getElementById("healthymind").style = "width: 100px; height: 150px; margin-top: 10px; margin-left: -70%;";
+            websymphonies.style = "width: 100px; height: 150px; margin-top: 10px; margin-left: 70%;";
+            epanos.style = "width: 100px; height: 150px; margin-top: 10px; margin-left: 25%;";
+            fluorescence.style = "width: 100px; height: 150px; margin-top: 10px; margin-left: -25%;";
+            healthymind.style = "width: 100px; height: 150px; margin-top: 10px; margin-left: -70%;";
+    
+            healthymind.style = "width: 100px; height: 150px; margin-left: -70%; margin-top: 5%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
+            fluorescence.style = "width: 100px; height: 150px; margin-left: -25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
+            epanos.style = "width: 100px; height: 150px; margin-left: 25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
+            websymphonies.style = "width: 100px; height: 150px; margin-left: 70%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
             
-            sleep(2000).then(() => {
-                document.getElementById("all").style = "width: 60%; margin-left: 20%;";
-                document.getElementById("healthymind").style = "width: 100px; height: 150px; margin-left: -70%; margin-top: 5%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
-                document.getElementById("fluorescence").style = "width: 100px; height: 150px; margin-left: -25%; margin-top: -13%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
-                document.getElementById("epanos").style = "width: 100px; height: 150px; margin-left: 25%; margin-top: -13%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
-                document.getElementById("websymphonies").style = "width: 100px; height: 150px; margin-left: 70%; margin-top: -13%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
-                sleep(1000).then(() => {
-                    enableScrolling();
-                    document.getElementById("healthymind").onclick = function() {
-                        document.getElementById("healthymind").style = "position: absolute; width: 100%; height: 100%; margin-top: -100px";
+            sleep(1000).then(() => {
+                enableScrolling();
+                /*
+                document.addEventListener('mousemove', (event) => {
+                    var w = event.clientX;
+                    healthymind.style = "transition: 0s; width: 100px; height: 150px; margin-left: -70%; margin-top: 5%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
+                    fluorescence.style = "transition: 0s; width: 100px; height: 150px; margin-left: -25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
+                    epanos.style = "transition: 0s; width: 100px; height: 150px; margin-left: 25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
+                    websymphonies.style = "transition: 0s; width: 100px; height: 150px; margin-left: 70%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4);";
+                    healthymind.style.backgroundPosition = (-1 * w/(window.innerWidth/120 + 160) - 75) + "px";
+                    fluorescence.style.backgroundPosition = (-1 * w/(window.innerWidth/120 + 160)) + "px";
+                    epanos.style.backgroundPosition = (-1 * w/(window.innerWidth/120 + 160) -175) + "px";
+                    websymphonies.style.backgroundPosition = (-1 * w/(window.innerWidth/120 + 160) - 113) + "px";
+                    
+                });
+                */     
+
+                healthymind.onmouseover = function(){
+                    healthymind.style = "cursor: pointer; width: 100px; height: 150px; margin-left: -70%; margin-top: 5%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1.1);";
+                    healthymind.onclick = function() {
                         document.getElementById("bg").style = "opacity: 1"
                         sleep(1000).then(() => {
                             location.href = "./healthymind.html";
                         });
                     }   
-                });
-                
+                };
+                healthymind.onmouseout = function() {
+                    healthymind.style = "cursor: default; width: 100px; height: 150px; margin-left: -70%; margin-top: 5%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1);";
+                }
+
+                fluorescence.onmouseover = function(){
+                    fluorescence.style = "cursor: pointer; width: 100px; height: 150px; margin-left: -25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1.1);";
+                    fluorescence.onclick = function() {
+                        document.getElementById("bg").style = "opacity: 1"
+                        sleep(1000).then(() => {
+                            location.href = "./fluorescence.html";
+                        });
+                    }   
+                };
+                fluorescence.onmouseout = function() {
+                    fluorescence.style = "cursor: default; width: 100px; height: 150px; margin-left: -25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1);";
+                }
+
+                epanos.onmouseover = function(){
+                    epanos.style = "cursor: pointer; width: 100px; height: 150px; margin-left: 25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1.1);";
+                    epanos.onclick = function() {
+                        document.getElementById("bg").style = "opacity: 1"
+                        sleep(1000).then(() => {
+                            location.href = "./healthymind.html";
+                        });
+                    }   
+                };
+                epanos.onmouseout = function() {
+                    epanos.style = "cursor: default; width: 100px; height: 150px; margin-left: 25%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1);";
+                }
+
+                websymphonies.onmouseover = function(){
+                    websymphonies.style = "cursor: pointer; width: 100px; height: 150px; margin-left: 70%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1.1);";
+                    websymphonies.onclick = function() {
+                        document.getElementById("bg").style = "opacity: 1"
+                        sleep(1000).then(() => {
+                            location.href = "./healthymind.html";
+                        });
+                    }   
+                };
+                websymphonies.onmouseout = function() {
+                    websymphonies.style = "cursor: default; width: 100px; height: 150px; margin-left: 70%; margin-top: -10%; box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); transform: scale(1);";
+                }
             });
+
          });
     }
     
--- universal.js
+++ universal.js
@@ -1,3 +1,4 @@
+AOS.init()
 function disableScrolling(){
     var x=window.scrollX;
     var y=window.scrollY;
@@ -17,3 +18,27 @@ window.onload = function(){document.getElementById("bg").style = "opacity: 0";};
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+document.getElementById("back").onclick = function() {
+    document.getElementById("bg").style = "opacity: 1";
+
+    sleep(1000).then(() => {
+        location.href = "./index.html";
+    })
+
+}
+
+document.getElementById("scrolltop").onclick = function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+var pagemain = document.getElementById("pagemain");
+setInterval(function () {
+    if (window.scrollY > 100) {
+        pagemain.style = "width: 70%; height: 400px;"; 
+    }
+    if (window.scrollY < 100) {
+        pagemain.style = "width: 100%; height: 55vh;"; 
+    }
+}, 10)
+
