cfb1b899ccac18cd8bd4819b055281c292188496
richardj-lin <richardlin4375@gmail.com>
2024-12-26 12:27:05 -0500

    Desktop Site Done

--- js/index.js
+++ js/index.js
@@ -1,79 +1,16 @@
-var offset;
-let width = window.screen.width;
-if (width >= 1024) {
-    offset = 100;
-}
-else {
-    offset=300;
-    
-}
-function scrollIndex() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-}
-function scrollAbout() {
-    var topAbout = document.getElementById("about").offsetTop - offset;
-    window.scrollTo({top: topAbout, behavior: 'smooth'});
-}
-function scrollWork() {
-    var topAbout = document.getElementById("work").offsetTop - offset;
-    window.scrollTo({top: topAbout, behavior: 'smooth'});
-}
-
 document.getElementById("email").onmouseover = function() {
-    document.getElementById("emails").style = "color: var(--text)";
-    document.getElementById("emails").innerHTML = "rlin@unc.edu  [Click to Copy]";
+    document.getElementById("email-copy").style="opacity: 100; margin-top: -30px;";
+    document.getElementById("email-copy").innerHTML = "rlin@unc.edu<br>[Click to copy]";
+};
 
-}
+document.getElementById("email").onmouseleave = function() {
+    document.getElementById("email-copy").style="color: var(--text); opacity: 0; margin-top: -50px;";
+};
 document.getElementById("email").onmousedown = function() {
     navigator.clipboard.writeText("rlin@unc.edu");
-    document.getElementById("emails").style = "color: var(--check)";
-    document.getElementById("emails").innerHTML = "rlin@unc.edu  [Copied!]";
-}
+    document.getElementById("email-copy").innerHTML = "rlin@unc.edu<br>[Copied!]";
+    document.getElementById("email-copy").style="color: var(--check); opacity: 100; margin-top: -30px;";
+};
 document.getElementById("linkedin").onmousedown = function() {
     window.open('https://www.linkedin.com/in/richardlin4375/', '_blank');
 }
-
-
-document.getElementById("link1").onmouseover = function() {
-    document.getElementById("linktext1").style="background-position: top left 100%;";
-    document.getElementById("linktext2").style="background-position: top right 100%;";
-    document.getElementById("linktext3").style="background-position: top right 100%;";
-    document.getElementById("linktext4").style="background-position: top right 100%;";
-    document.getElementById("linktext5").style="background-position: top right 100%;";
-    document.getElementById("work-img").src = "https://youthhealthymind.com/images/unsplash-hike.jpg";
-};
-document.getElementById("link2").onmouseover = function() {
-    document.getElementById("linktext1").style="background-position: top right 100%;";
-    document.getElementById("linktext2").style="background-position: top left 100%;";
-    document.getElementById("linktext3").style="background-position: top right 100%;";
-    document.getElementById("linktext4").style="background-position: top right 100%;";
-    document.getElementById("linktext5").style="background-position: top right 100%;";
-    document.getElementById("work-img").src = "https://cansforkidsnc.org/images/canholding.jpg";
-};
-document.getElementById("link3").onmouseover = function() {
-    document.getElementById("linktext1").style="background-position: top right 100%;";
-    document.getElementById("linktext2").style="background-position: top right 100%;";
-    document.getElementById("linktext3").style="background-position: top left 100%;";
-    document.getElementById("linktext4").style="background-position: top right 100%;";
-    document.getElementById("linktext5").style="background-position: top right 100%;";
-    document.getElementById("work-img").src = "https://thepowcenter.com/images/kenjibed.jpg";
-};
-document.getElementById("link4").onmouseover = function() {
-    document.getElementById("linktext1").style="background-position: top right 100%;";
-    document.getElementById("linktext2").style="background-position: top right 100%;";
-    document.getElementById("linktext3").style="background-position: top right 100%;";
-    document.getElementById("linktext4").style="background-position: top left 100%;";
-    document.getElementById("linktext5").style="background-position: top right 100%;";
-    document.getElementById("work-img").src = "https://tsapulsar.github.io/images/earth.jpg";
-};
-document.getElementById("link5").onmouseover = function() {
-    document.getElementById("linktext1").style="background-position: top right 100%;";
-    document.getElementById("linktext2").style="background-position: top right 100%;";
-    document.getElementById("linktext3").style="background-position: top right 100%;";
-    document.getElementById("linktext4").style="background-position: top right 100%;";
-    document.getElementById("linktext5").style="background-position: top left 100%;";
-    document.getElementById("work-img").src = "https://fluorescencetsa.github.io/images/balloons.jpg";
-};
-
-
-
--- js/universal.js
+++ js/universal.js
@@ -1,1 +1,38 @@
+/*document.getElementById("menu-home").onmousedown = function() {
+    window.open('./index.html', target="_self");
+};
+document.getElementById("menu-about").onmousedown = function() {
+    window.open('./about.html', target="_self");
+};
+document.getElementById("menu-work").onmousedown = function() {
+    window.open('./work.html', target="_self");
+};*/
 
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
+
+
+let width = window.screen.width;
+if (width > 1024) {
+    setInterval(function () {
+    if (window.scrollY <= 500) {
+        document.getElementById("overlay-menu").style = "opacity: 0; margin-top: -50px;";
+    }
+    else {
+        document.getElementById("overlay-menu").style = "opacity: 100; margin-top: 15px;";
+    
+    }
+}, 10)
+}
+else {
+    document.getElementById("title").innerHTML = "Youth HealthyMind";
+}
